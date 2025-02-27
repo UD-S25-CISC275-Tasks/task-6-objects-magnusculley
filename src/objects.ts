4a93f7f8734592486b5004640d754df9c803a4e9
magnus <magnus@udel.edu>
2025-02-27 18:20:41 -0500

    added very awesomely compact function isValid

--- src/objects.ts
+++ src/objects.ts
@@ -46,7 +46,13 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") {
+        return true;
+    } else {
+        return question.options.some(
+            (option: string): boolean => option === answer,
+        );
+    }
 }
 
 /**
